f851ade94e0e96ed5a617e4c020fb12811bdd9a4
Adam Dangoor <adamdangoor@gmail.com>
2019-04-25 19:19:03 +0100

    Passing tests

--- tests/test_pathlib.py
+++ tests/test_pathlib.py
@@ -1,7 +1,7 @@
-import click
 from pathlib import Path
-# import click_pathlib
 
+import click
+import click_pathlib
 from click.testing import CliRunner
 
 
@@ -9,18 +9,20 @@ def test_example(tmp_path: Path) -> None:
     @click.command('delete')
     @click.argument(
         'existing_file',
-        # type=click_pathlib.Path(exists=True),
+        type=click_pathlib.Path(exists=True),
     )
     def delete(existing_file: Path) -> None:
         assert isinstance(existing_file, Path)
         existing_file.unlink()
 
+    tmp_file = tmp_path / 'new.txt'
+    tmp_file.touch()
+    assert tmp_file.exists()
     runner = CliRunner()
     result = runner.invoke(
         delete,
-        [str(tmp_path)],
+        [str(tmp_file)],
         catch_exceptions=False,
     )
-    import pdb; pdb.set_trace()
     assert result.exit_code == 0
-    assert not tmp_path.exists()
+    assert not tmp_file.exists()
