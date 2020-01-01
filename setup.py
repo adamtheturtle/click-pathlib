448711a4e3240836cc3dc30fb98e242c64c84c6a
Adam Dangoor <adamdangoor@gmail.com>
2020-01-01 13:34:37 +0000

    Do not set version file

--- setup.py
+++ setup.py
@@ -27,9 +27,7 @@ DEV_REQUIRES = _get_dependencies(
 )
 
 setup(
-    use_scm_version={
-        'write_to': 'src/click_pathlib/_setuptools_scm_version.txt',
-    },
+    use_scm_version=True,
     setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
     install_requires=INSTALL_REQUIRES,
     extras_require={'dev': DEV_REQUIRES},
