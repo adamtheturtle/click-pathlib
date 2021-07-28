fc1872244050e89cd77bfbe216755924eb3b82f3
Adam Dangoor <adamdangoor@gmail.com>
2021-07-28 10:09:36 +0100

    Move requirements to a requirements directory

--- lint.mk
+++ lint.mk
@@ -36,11 +36,11 @@ fix-isort:
 
 .PHONY: pip-extra-reqs
 pip-extra-reqs:
-	pip-extra-reqs src/
+	pip-extra-reqs --requirements-file=requirements/requirements.txt src/
 
 .PHONY: pip-missing-reqs
 pip-missing-reqs:
-	pip-missing-reqs src/
+	pip-missing-reqs --requirements-file=requirements/requirements.txt src/
 
 .PHONY: pylint
 pylint:
